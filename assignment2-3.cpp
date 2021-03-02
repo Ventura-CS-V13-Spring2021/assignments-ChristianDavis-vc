24c38e4a2df9e937f026fd7a61433c312c997144
Kyuwoong Lee <kwooler@gmail.com>
2021-03-02 16:57:48 +0000

    Kyu

---
+++ assignment2-3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char  selection;
+	cout << "Enter your choice among A, B, C\n";
+	cin >> selection;
+	// if (selection == 'A' || selection == 'a')
+	// 	cout << "Your choice is A\n";
+    // else if (selection == 'B' || selection == 'b')
+    //     cout << "Your choice is B\n";
+    // else if (selection == 'C' || selection == 'c')
+    //      cout << "Your choice is C\n";
+	// else
+	// 	cout << "Not good choice\n";
+	// 	return 0;
+
+// switch (main)
+switch (selection)
+{
+//   case 1:
+  case 'a':
+  case 'A':
+    cout << "your choice is A";
+    break;
+  case 2: 
+    cout << "your choice is B";
+    break;
+  case 3: 
+    cout << "your choice is C";
+    break;
+  default:
+    cout << "unknown choice";
+ }
+
+
+
+
