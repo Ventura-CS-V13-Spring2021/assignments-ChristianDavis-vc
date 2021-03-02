a425ba48ae87a1614a160a53a6ea47cb4c493cd3
Christian  Davis <cd148065@gmail.com>
2021-03-02 01:21:16 +0000

    found duplicated numbers

---
+++ assignment2-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int number1, number2, number3;
+  cout << "Enter three integer values : " ;
+  cin >> number1 >> number2 >> number3 ;
+
+if (number1 == number2 && number2 == number3)
+  cout << "All numbers are the same : " ;
+
+ return 0;
+}
